e00fb8cc8de4b0f032322679f74c5d528447fbcc
Abdullah Mukhlashin <abdullahmukhlashiin@gmail.com>
2020-07-29 22:24:15 +0800

    Initial commit

---
+++ app/src/main/java/cmd/ushiramaru/weap/activities/ConversationActivity.kt
@@ -0,0 +1,13 @@
+package cmd.ushiramaru.weap.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cmd.ushiramaru.weap.R
+
+class ConversationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_conversation)
+    }
+}
---
+++ app/src/main/java/cmd/ushiramaru/weap/fragments/StatusListFragment.kt
@@ -0,0 +1,23 @@
+package cmd.ushiramaru.weap.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import cmd.ushiramaru.weap.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class StatusListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_status_list, container, false)
+    }
+
+}
